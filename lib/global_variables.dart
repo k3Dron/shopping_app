090cb050b5cac8134e14542d15ff7e413c250bcb
Your Name <you@example.com>
2024-01-22 13:33:01 +0530

    First Commit

---
+++ lib/global_variables.dart
@@ -0,0 +1,34 @@
+final products = [
+  {
+    'id': '0',
+    'title': 'Men\'s Nike Shoes',
+    'price': 44.52,
+    'imageUrl': 'assets/images/nike.png',
+    'company': 'Nike',
+    'sizes': [9, 10, 11, 12],
+  },
+  {
+    'id': '1',
+    'title': 'Addidas Shoes',
+    'price': 20.12,
+    'imageUrl': 'assets/images/addidas.png',
+    'company': 'Addidas',
+    'sizes': [9, 10, 12],
+  },
+  {
+    'id': '2',
+    'title': 'Bata Women\'s Shoes',
+    'price': 28.95,
+    'imageUrl': 'assets/images/bata.png',
+    'company': 'Bata',
+    'sizes': [8, 9, 10],
+  },
+  {
+    'id': '3',
+    'title': 'Jordan Shoes',
+    'price': 420.69,
+    'imageUrl': 'assets/images/jordan.png',
+    'company': 'Nike',
+    'sizes': [8, 9, 10],
+  },
+];
